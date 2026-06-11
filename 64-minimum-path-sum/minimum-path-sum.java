4656eed1bd7b4389b3e11aab9a3cef0d3c998582
Utkarsh <2k22.cse.2212990@gmail.com>
2026-06-12 00:49:11 +0530

    Time: 4 ms (62.98%) | Memory: 50.5 MB (21.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,25 +1,18 @@
 class Solution {
-    int[][] memo;
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        memo = new int[m][n];
-        for (int[] rows : memo) {
-            Arrays.fill(rows, -1);
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0  &&  j == 0) dp[i][j] = grid[i][j];
+                else if (i == 0) dp[i][j] = grid[i][j] + dp[i][j - 1];
+                else if (j == 0) dp[i][j] = grid[i][j] + dp[i - 1][j];
+                else dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
 
-        return minPath(0, 0, m, n, grid);
-    }
-
-    private int minPath(int row, int col, int m, int n, int[][]grid) {
-        if (row >= m  ||  col >= n) return Integer.MAX_VALUE;
-        if (row == m - 1  && col == n - 1) return grid[row][col];
-
-        if (memo[row][col] != -1) return memo[row][col];
-
-        int down = minPath(row + 1, col, m, n, grid);
-        int right = minPath(row, col + 1, m, n, grid);
-
-        return memo[row][col] = grid[row][col] + Math.min(right, down);
+        return dp[m - 1][n - 1];
     }
 }
